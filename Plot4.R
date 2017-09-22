d1b8a308732634b73c09a78dc85cc31b4bb39462
Rémi Bacha <remi.bacha@corp.ovh.com>
2017-09-22 09:08:16 +0200

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,3 +1,16 @@
+setwd("~/Documents/Scripts_R/Coursera")
+fileURL<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL, destfile = "~/Documents/Scripts_R/Coursera/household_power_consumption.zip", method = "curl")
+unzip("household_power_consumption.zip")
+dataset <- read.table("household_power_consumption.txt",header=TRUE,sep = ";", na.strings = "?")
+
+dataset$Date <- as.Date(dataset$Date,"%d/%m/%Y")
+dataset$Time <- as.character(dataset$Time)
+dataset[, c(3,4,5,6,7,8,9)] <- sapply(dataset[, c(3,4,5,6,7,8,9)], as.numeric)
+filtered_dataset <- subset(dataset, Date>="2007-02-01" & Date<="2007-02-02") 
+filtered_dataset <- filtered_dataset[complete.cases(filtered_dataset),]
+filtered_dataset$Date_Time <- as.POSIXct(paste(filtered_dataset$Date, filtered_dataset$Time), format="%Y-%m-%d %H:%M:%S")
+
 #Plot 4
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 
